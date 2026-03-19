c1b2000b7a3168600ab56f8c1025f5942a997f75
WaveringRuby <m.g.m.depaula.mgmdp@gmail.com>
2026-03-18 23:27:14 -0300

    Integrado o backend com o frontend
    Foi integrado o backend com o frontend. Criado um template básico para personagem (será descontinuado) e também foi editado os arquivos necessários

--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -1,18 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "../pages/Home"
-import Characters from "../pages/Characters"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home.jsx";
+import Characters from "../pages/Characters.jsx";
 
 export default function Router() {
 
- return (
-  <BrowserRouter>
-   <Routes>
-
-    <Route path="/" element={<Home/>}/>
-    <Route path="/characters" element={<Characters/>}/>
-
-   </Routes>
-  </BrowserRouter>
- )
-
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/characters" element={<Characters/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
