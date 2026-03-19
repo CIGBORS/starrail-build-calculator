260266f27df573fbd6c75f6339620d9d37724d83
WaveringRuby <m.g.m.depaula.mgmdp@gmail.com>
2026-03-19 17:34:33 -0300

    Ajustado uma parte do service do backend
    Criação do começo da API geral, fazendo ligação com as demais tabelas dentro do backend

--- backend/src/services/starRail.service.js
+++ backend/src/services/starRail.service.js
@@ -3,6 +3,11 @@ import axios from "axios"; // Para manipular os json vindo direto do GitHub
 // Esse link está relativo ao código do personagem
 const GITHUB_URL = "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/";
 
+async function fetchJson(endpoint){
+    const response = await axios.get(`${GITHUB_URL}index_new/pt/${endpoint}`);
+    return response.data;
+}
+
 export function getCharacterById(characterId){
     return axios.get(`${GITHUB_URL}index_new/pt/characters.json`) // Lembrando que setei PT por que nós somos brasileiros
         .then(response => {
@@ -22,31 +27,41 @@ export function getCharacterById(characterId){
         });
 }
 
-export function getCharacterAllInformations(characterId){
-    // Essa função é exclusiva para os personagens que são, em especial, do modo de jogo normal.
-    const characterData = axios.get(`${GITHUB_URL}index_new/pt/characters.json`)
-        .then(response => {
-            const characters = response.data;
-            const characterData = characters[characterId];
+export async function getCharacterAllInformations(characterId){
+    // Essa função é exclusiva para os personagens que são, em especial, do modo de jogo normal
+    const charactersData = await fetchJson("characters.json");
+    const characterData = charactersData[characterId];
 
-            if(!characterData){
-                return null;
-            }
+    if(!characterData){
+        return null;
+    }
 
-            return {
-                id: characterId,
-                name: characterData.name,
-                rarity: characterData.rarity,
-                element: characterData.element,
-                max_sp: characterData.max_sp,
-                ranks: characterData.ranks,
-                skills: characterData.skills,
-                skills_trees: characterData.skills_trees,
-                icon: `${GITHUB_URL}${characterData.icon}`,
-                preview: `${GITHUB_URL}${characterData.preview}`,
-                portrait: `${GITHUB_URL}${characterData.portrait}`
-            }
-        });
+    // Relacionamento com os elementos do personagem
+    const elementsData = await fetchJson("elements.json");
+    const elementData = elementsData[characterData.element];
 
-    return characterData;
+    // Convertendo o ícone para a foto correta
+    elementData.icon = `${GITHUB_URL}${elementData.icon}`;
+
+    // Relacionamento com as habilidades do personagem escolhido
+    const skillsData = await fetchJson("character_skills.json");
+
+    const characterSkills = characterData.skills.map(skillId => ({
+        id: skillId,
+        ...skillsData[skillId]
+    }));
+
+    return {
+        id: characterId,
+        name: characterData.name,
+        rarity: characterData.rarity,
+        element: elementData,
+        max_sp: characterData.max_sp,
+        ranks: characterData.ranks,
+        skills: characterSkills,
+        skills_trees: characterData.skills_trees,
+        icon: `${GITHUB_URL}${characterData.icon}`,
+        preview: `${GITHUB_URL}${characterData.preview}`,
+        portrait: `${GITHUB_URL}${characterData.portrait}`
+    }
 }
