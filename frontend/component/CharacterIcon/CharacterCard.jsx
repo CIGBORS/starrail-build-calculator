120d48ca8e6f90388cb031ce55d1ff1fb78282c5
WaveringRuby <m.g.m.depaula.mgmdp@gmail.com>
2026-04-03 19:34:57 -0300

    Criado o template básico do card do personagem
    O template está conforme o figma, porém aind não foi testado com imagens do backend. Porém todas as configurações já estão conforme foram criadas no figma.

--- frontend/component/CharacterIcon/CharacterCard.jsx
+++ frontend/component/CharacterIcon/CharacterCard.jsx
@@ -1,13 +1,22 @@
 import React from "react";
-import "CharacterCard.css";
+import "./CharacterCard.css";
 
-export default function CharacterCard({ character_name = "Clara", character_image = "fodase", character_element = "", character_path = "" }) {
+export default function CharacterCard({ character_name = "Clara", character_image = "vazio", character_element = "vazio", character_path = "vazio" }) {
 
     return(
         <>
             <div className="character-card__component">
+                <img src={character_image} className="character-image" alt={character_name} />
+
                 <div className="character-card__header">
-                    <img src={character_image} alt={character_name} />
+                    <img src={character_element} alt={character_name} />
+                    <img src={character_path} alt={character_name} />
+                </div>
+
+                <div className="character_footer">
+                    <h1 className="character_name">
+                        {character_name}
+                    </h1>
                 </div>
             </div>
         </>
