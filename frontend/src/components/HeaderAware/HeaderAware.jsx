f3a3e022c3ba1a75e87d8abe3e1f861b7420510e
WaveringRuby <m.g.m.depaula.mgmdp@gmail.com>
2026-04-04 18:36:16 -0300

    Criado o código para o texto do aware no menu

--- frontend/src/components/HeaderAware/HeaderAware.jsx
+++ frontend/src/components/HeaderAware/HeaderAware.jsx
@@ -1,6 +1,13 @@
+import "./HeaderAware.css";
+
 export default function HeaderAware () {
     return (
         <>
+            <div className="header-aware">
+                <div className="header-text">
+                    <b><i>STAR RAIL BUILDER </i></b> is an independent and fan-made website. All trademarks, game content and copyrights belong to thei respective owners. If you need further information, it can be found here: <b className="yellow">ABOUT STAR RAIL BUILDER | Privacy Policy</b>
+                </div>
+            </div>
         </>
     )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,25 @@
+import HeaderAware from "../components/HeaderAware/HeaderAware";
 import LateralBar from "../components/LateralBar/LateralBar";
 
 export default function Home(){
 
     return (
         <> 
-            <img className="home-banner" src="banners/home-banner.png" />
-            <LateralBar />
+            <div className="home-container">
+                <img className="home-banner" src="banners/home-banner.png" /> 
+
+                <div className="home-content">
+                    
+                    <LateralBar />
+                
+                    <div className="home-main-content">
+                       
+                        {/* Uso futuro */}
+                    </div>
+                </div>
+
+                <HeaderAware /> 
+            </div>
         </>
     )
 }
