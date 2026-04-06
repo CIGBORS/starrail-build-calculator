223d3baea37644d17b1f59d32da4a7e31df494d5
WaveringRuby <m.g.m.depaula.mgmdp@gmail.com>
2026-04-06 18:38:57 -0300

    Organizado a tela de personagens
    Para a organização foi adicionado a barra lateral e organizado a página de personagem.

--- frontend/src/pages/Characters.jsx
+++ frontend/src/pages/Characters.jsx
@@ -1,9 +1,13 @@
 import CharacterCardList from "../components/CharacterCardList/CharacterCardList";
+import LateralBar from "../components/LateralBar/LateralBar";
 
 export default function Characters(){
     return (
-        <>
-            <CharacterCardList />
+        <> 
+            <LateralBar />
+            <div className="characters-content">
+                <CharacterCardList />
+            </div>
         </>
     )
 }
