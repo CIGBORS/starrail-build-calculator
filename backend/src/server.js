59d5d981e667ea4f0e23bb650990fa1000a76018
FlpGamess <pereirafelipe2003@hotmail.com>
2026-04-11 21:46:20 -0300

    Conexão com o Redis e Docker
    estou subindo o redisClient responsavel por conectar com o docker  (precisa ser baixado por fora no pc)
    alem disso no .env do backend é preciso por o REDIS_URL para a conexão

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,28 +1,30 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express, { json } from "express"
-import cors from "cors"
+import express, { json } from "express";
+import cors from "cors";
 
-import testRoutes from "./routes/test.routes.js"
+import testRoutes from "./routes/test.routes.js";
 import charactersRoutes from "./routes/starRail/characters.routes.js";
 import loginRoutes from "./routes/login/login.routes.js";
 
-const app = express()
+import redis from "../redis/redisClient.js";
 
-app.use(cors())
-app.use(json())
+const app = express();
 
-app.use("/test", testRoutes)
+app.use(cors());
+app.use(json());
+
+app.use("/test", testRoutes);
 app.use("/api/github/characters", charactersRoutes);
 app.use("/api/login", loginRoutes);
 
-app.get("/", (req,res)=>{
- res.json({message:"API funcionando"})
-})
+app.get("/", (req, res) => {
+  res.json({ message: "API funcionando" });
+});
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
- console.log(`Servidor rodando na porta ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
