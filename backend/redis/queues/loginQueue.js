4af84e1eb4ccba1b1c5a4d1ff5920281a30047ed
FlpGamess <pereirafelipe2003@hotmail.com>
2026-04-12 15:52:45 -0300

    Comentando as linhas do redis
    comentei os comandos do redis para entendimento

--- backend/redis/queues/loginQueue.js
+++ backend/redis/queues/loginQueue.js
@@ -1,9 +1,19 @@
+//aqui fica a fila pro login
+//importa o client vulgo redis
 import redis from "../redisClient.js";
 
+//função para contar os usuarios logados
 export async function countUsersLogin(data) {
-  await redis.xAdd("login-stream", "*", {
-    username: data.username,
-    type: data.type,
-    timestamp: Date.now().toString(),
-  });
+  //cria um evento na fila
+  //adicionar mensagem na fila login-stream
+  await redis.xAdd(
+    "login-stream",
+    "*",
+    //mensagem enviada
+    {
+      username: data.username,
+      type: data.type,
+      timestamp: Date.now().toString(),
+    },
+  );
 }
--- backend/redis/worker/loginWorker.js
+++ backend/redis/worker/loginWorker.js
@@ -1,25 +1,41 @@
+//importa redis
 import redis from "../../redis/redisClient.js";
 
+//worker é como um loop infinito
 async function startWorker() {
   console.log("Login worker iniciado");
-
+  //worker é executado o tempo todo
+  //sempre se comunica com a fila
   while (true) {
+    //recebe a fila, lendo seus eventos
     const response = await redis.xRead(
       [
+        //key: fila
+        //id: ultimos eventos
+        //observa e reage quando surgir um novo evento na fila
         {
           key: "login-stream",
           id: "$",
         },
       ],
       {
+        //block  0 significa que ele espera uma resposta infinitamente
+        //fica esperando o login acontecer para reagir
         BLOCK: 0,
       },
     );
 
+    //quando tiver login
     if (response) {
+      //redis pode mandar varias mensagens
       const stream = response[0].messages;
 
+      //processa cada login
       for (const message of stream) {
+        //pega os dados
+        //no caso do login seria
+        //  username:--
+        //  type: login
         const data = message.message;
 
         console.log("Login recebido:", data);
@@ -27,10 +43,11 @@ async function startWorker() {
         // Incrementa contador
         await redis.incr("users_logged");
 
+        //log final que mostra no console
         console.log("Usuarios logados:", await redis.get("users_logged"));
       }
     }
   }
 }
-
+//inicia o worker
 startWorker();
