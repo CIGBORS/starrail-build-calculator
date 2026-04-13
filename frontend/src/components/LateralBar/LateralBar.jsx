1dfb8b750e901d0b48f2bfcf080227b5f1e664cc
FlpGamess <pereirafelipe2003@hotmail.com>
2026-04-12 21:23:44 -0300

    Adicionado o frontend no docker
    adicionado o frontend no docker, agora é só dar o docker compose na raiz do projeto

--- frontend/src/components/LateralBar/LateralBar.jsx
+++ frontend/src/components/LateralBar/LateralBar.jsx
@@ -14,38 +14,82 @@ export default function LateralBar({ userLogged = null }) {
     <>
       <div className="lateral-bar">
         {/* Adicionar condições para caso o usuário esteja loggado*/}
-        {
-            userLogged === null ? 
-            ( <img className="lateral-bar__user-icon" src="icons/place_holder.png" /> ) :
-            ( <img src={userIcon} />)
-        }
+        {userLogged === null ? (
+          <img
+            className="lateral-bar__user-icon"
+            src="icons/place_holder.png"
+          />
+        ) : (
+          <img src={userIcon} />
+        )}
 
         <div className="lateral-bar__primary-icons">
-            <div className="primary-icons__icon">
-                <NavLink to="/characters" className={({isActive}) => isActive ? "nav-link-active" : ""}>
-                    <img className="lateral-bar__icon" src="icons/character-icon.png" />
-                </NavLink>
-                
-                <NavLink to="/relics" className={({isActive}) => isActive ? "nav-link-active" : ""}>
-                    <img className="lateral-bar__icon" src="icons/relic-icon.png" />
-                </NavLink>
+          <div className="primary-icons__icon">
+            <NavLink
+              to="/characters"
+              className={({ isActive }) => (isActive ? "nav-link-active" : "")}
+            >
+              <img
+                className="lateral-bar__icon"
+                src="icons/character-icon.png"
+              />
+            </NavLink>
 
-                <NavLink to="/light-cones" className={({isActive}) => isActive ? "nav-link-active" : ""}>
-                    <img className="lateral-bar__icon" src="icons/light-cone-icon.png" />
-                </NavLink>
+            {/* <NavLink
+              to="/relics"
+              className={({ isActive }) => (isActive ? "nav-link-active" : "")}
+            >*/}
+            <img
+              className="lateral-bar__icon"
+              src="icons/relic-icon.png"
+              onClick={() => alert("Em desenvolvimento")}
+            />
+            {/*  </NavLink>*/}
+            {/*
+            <NavLink
+              to="/light-cones"
+              className={({ isActive }) => (isActive ? "nav-link-active" : "")}
+            >*/}
+            <img
+              className="lateral-bar__icon"
+              src="icons/light-cone-icon.png"
+              onClick={() => alert("Em desenvolvimento")}
+            />
+            {/*</NavLink>*/}
 
-                <NavLink to="/saved-builds" className={({isActive}) => isActive ? "nav-link-active" : ""}>
-                    <img className="lateral-bar__icon" src="icons/team-icon.png" />
-                </NavLink>
+            {/*<NavLink
+              to="/saved-builds"
+              className={({ isActive }) => (isActive ? "nav-link-active" : "")}
+            >*/}
+            <img
+              className="lateral-bar__icon"
+              src="icons/team-icon.png"
+              onClick={() => alert("Em desenvolvimento")}
+            />
+            {/*</NavLink>*/}
 
-                <NavLink to="/build-creators" className={({isActive}) => isActive ? "nav-link-active" : ""}>
-                    <img className="lateral-bar__icon" src="icons/inventory-icon.png" />
-                </NavLink>
+            {/*<NavLink
+              to="/build-creators"
+              className={({ isActive }) => (isActive ? "nav-link-active" : "")}
+            >*/}
+            <img
+              className="lateral-bar__icon"
+              src="icons/inventory-icon.png"
+              onClick={() => alert("Em desenvolvimento")}
+            />
+            {/*</NavLink>*/}
 
-                <NavLink to="/general" className={({isActive}) => isActive ? "nav-link-active" : ""}>
-                    <img className="lateral-bar__icon" src="icons/book-icon.png" />
-                </NavLink>
-            </div>
+            {/* <NavLink
+              to="/general"
+              className={({ isActive }) => (isActive ? "nav-link-active" : "")}
+            >*/}
+            <img
+              className="lateral-bar__icon"
+              src="icons/book-icon.png"
+              onClick={() => alert("Em desenvolvimento")}
+            />
+            {/*</NavLink>*/}
+          </div>
         </div>
 
         <div className="lateral-bar__footer">
