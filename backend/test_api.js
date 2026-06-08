82b37b5daaee46680de93aea1fcbaa1653fd5394
FlpGamess <pereirafelipe2003@hotmail.com>
2026-06-08 13:54:45 -0300

    Update test_api.js

--- backend/test_api.js
+++ backend/test_api.js
@@ -1,6 +1,8 @@
 async function run() {
     try {
-        const res = await fetch("http://localhost:3000/api/github/calculator/delete-build/1");
+        const res = await fetch("http://localhost:3000/api/github/calculator/delete-build/1", {
+            method: 'DELETE'
+        });
         const data = await res.json();
         console.log("Status:", res.status);
         console.log("Data:", data);
